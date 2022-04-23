b6e266ef43a95288790442f4a9646250700bef8b
anjuaji92 <97083740+anjuaji92@users.noreply.github.com>
2022-04-23 19:02:21 +0530

    Update app.js

--- app.js
+++ app.js
@@ -1,8 +1,8 @@
 const express=require('express');
 const app= new express;
-var path = require('path');
+//var path = require('path');
 
-app.use(express.static(path.join(__dirname, '/public')));
+//app.use(express.static(path.join(__dirname, '/public')));
 app.set('view engine','ejs');
 app.set('views','./src/views')
 app.get('/',function(req,res){
